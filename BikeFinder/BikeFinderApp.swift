460b65472a70fdd556fed85447b29c983507858d
Julia Ju <68553919+jooliaju@users.noreply.github.com>
2023-09-05 15:12:00 +0200

    Initial Commit

---
+++ BikeFinder/BikeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeFinderApp.swift
+//  BikeFinder
+//
+//  Created by Julia Ju on 2023-09-05.
+//
+
+import SwiftUI
+
+@main
+struct BikeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
